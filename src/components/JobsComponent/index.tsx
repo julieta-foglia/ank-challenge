84fb9056e6ec8827d696cecceea0486dd322372c
julietafoglia <julieta.foglia@valtech.com>
2021-01-07 16:09:41 -0300

    Fix bug

--- src/components/JobsComponent/index.tsx
+++ src/components/JobsComponent/index.tsx
@@ -5,35 +5,35 @@ import { Title } from './styled';
 import { JobItem } from 'types/jobItem';
 
 interface JobsComponentProps {
-    jobs: JobItem[];
-    company?: string;
+  jobs: JobItem[];
+  company?: string;
 }
 
 const JobsComponent: React.FC<JobsComponentProps> = ({ jobs, company }: JobsComponentProps) => {
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
-    const renderItem = ({ item }) => (
-        <JobListItem
-            id={item.id}
-            title={item.title}
-            cities={item.cities}
-            countries={item.countries}
-            commitment={item.commitment.title}
-            remote={item.remotes}
-            onPress={() =>
-                navigation.navigate('Job Application', {
-                    job: item.title,
-                })
-            }
-        />
-    );
+  const renderItem = ({ item }) => (
+    <JobListItem
+      id={item.id}
+      title={item.title}
+      cities={item.cities}
+      countries={item.countries}
+      commitment={item.commitment.title}
+      remote={item.remotes}
+      onPress={() =>
+        navigation.navigate('Job Application', {
+          job: item.title,
+        })
+      }
+    />
+  );
 
-    return (
-        <>
-            <Title>Jobs for {company}</Title>
-            <List data={jobs} renderItem={renderItem} />
-        </>
-    );
+  return (
+    <>
+      {company ? <Title>Jobs for {company}</Title> : <Title>Faved Jobs</Title>}
+      <List data={jobs} renderItem={renderItem} />
+    </>
+  );
 };
 
 export default JobsComponent;
--- src/screens/FavedJobs/index.tsx
+++ src/screens/FavedJobs/index.tsx
@@ -7,60 +7,60 @@ import { REMOVE_ALL_TEXT } from '../../config/constants/texts';
 import { isNonEmptyArray } from '@apollo/client/utilities';
 
 const FavedJobs: React.FC = () => {
-    const [favs, setFavs] = useState([]);
+  const [favs, setFavs] = useState([]);
 
-    useEffect(() => {
-        getFavorites();
-    }, [favs]);
+  useEffect(() => {
+    getFavorites();
+  }, [favs]);
 
-    const getFavorites = async (): Promise<void> => {
-        try {
-            const jsonValue = await AsyncStorage.getItem('faved-jobs');
-            setFavs(JSON.parse(jsonValue) || []);
-        } catch (e) {
-            // read key error
-        }
-    };
+  const getFavorites = async (): Promise<void> => {
+    try {
+      const jsonValue = await AsyncStorage.getItem('faved-jobs');
+      setFavs(JSON.parse(jsonValue) || []);
+    } catch (e) {
+      // read key error
+    }
+  };
 
-    const confirmAction = (): void => {
-        Alert.alert(
-            'Remove All',
-            REMOVE_ALL_TEXT,
-            [
-                {
-                    text: 'Cancel',
-                    onPress: () => console.log('Cancel Pressed'),
-                    style: 'cancel',
-                },
-                { text: 'OK', onPress: () => removeFavorites() },
-            ],
-            { cancelable: false },
-        );
-    };
-
-    const removeFavorites = async (): Promise<void> => {
-        try {
-            await AsyncStorage.removeItem('faved-jobs');
-            getFavorites();
-        } catch (error) {
-            console.log(error);
-        }
-    };
-
-    return (
-        <Wrapper>
-            {isNonEmptyArray(favs) ? (
-                <>
-                    <JobsComponent jobs={favs} />
-                    <ButtonWrapper onPress={() => confirmAction()}>
-                        <RemoveButtonText>Remove All</RemoveButtonText>
-                    </ButtonWrapper>
-                </>
-            ) : (
-                <Title>There are no faved jobs</Title>
-            )}
-        </Wrapper>
+  const confirmAction = (): void => {
+    Alert.alert(
+      'Remove All',
+      REMOVE_ALL_TEXT,
+      [
+        {
+          text: 'Cancel',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel',
+        },
+        { text: 'OK', onPress: () => removeFavorites() },
+      ],
+      { cancelable: false },
     );
+  };
+
+  const removeFavorites = async (): Promise<void> => {
+    try {
+      await AsyncStorage.removeItem('faved-jobs');
+      getFavorites();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <Wrapper>
+      {isNonEmptyArray(favs) ? (
+        <>
+          <JobsComponent jobs={favs} />
+          <ButtonWrapper onPress={() => confirmAction()}>
+            <RemoveButtonText>Remove All</RemoveButtonText>
+          </ButtonWrapper>
+        </>
+      ) : (
+        <Title>There are no faved jobs</Title>
+      )}
+    </Wrapper>
+  );
 };
 
 export default FavedJobs;
